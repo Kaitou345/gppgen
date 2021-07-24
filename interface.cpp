54c94ab08c28d976c79a0fbd6a4a845a473584e4
Kaitou345 <68972628+Kaitou345@users.noreply.github.com>
2021-07-25 00:31:45 +0600

    Add files via upload

---
+++ interface.cpp
@@ -0,0 +1,112 @@
+#include "interface.h"
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::string>    Interface::s_SourceDirs;		// 	1
+Standard 				    Interface::s_Standard;			// 	2
+std::vector<std::string>    Interface::s_IncludeDirs;		// 	3	
+std::vector<std::string>    Interface::s_LibDirs;			// 	4
+std::vector<std::string>    Interface::s_LibLinks;	  		//	5	
+bool 					    Interface::s_DebugFlag;         //  6
+std::vector<std::string>    Interface::s_Defines;			// 	7
+std::string 			    Interface::s_OutputDir;         //	8
+std::string 			    Interface::s_OutputName;	    // 	9
+
+const std::vector<std::string>& Interface::GetSourceDirs()  { return s_SourceDirs; }    // 1
+const std::vector<std::string>& Interface::GetIncludeDirs() { return s_IncludeDirs; }   // 3
+const std::vector<std::string>& Interface::GetLibDirs()     { return s_LibDirs; }       // 4
+const std::vector<std::string>& Interface::GetLibLinks()    { return s_LibLinks; }      // 5
+const std::vector<std::string>& Interface::GetDefines()     { return s_Defines; }       // 7
+const bool                      Interface::GetDebugFlag()   { return s_DebugFlag; }     // 6
+const std::string&              Interface::GetOutputDir()   { return s_OutputDir; }     // 8
+const std::string&              Interface::GetOutputName()  { return s_OutputName; }    // 9
+
+const std::string               Interface::GetStandard()                                // 6
+{
+    switch (s_Standard)
+    {
+    case Standard::CPP98:
+        return "C++98";
+        break;
+    case Standard::CPP03:
+        return "C++03";
+        break;
+    case Standard::CPP11:
+        return "C++11";
+        break;
+    case Standard::CPP14:
+        return "C++14";
+        break;
+    case Standard::CPP17:
+        return "C++17";
+        break;
+    case Standard::CPP20:
+        return "C++20";
+        break;
+    case Standard::CPP23:
+        return "C++23";
+        break;
+    default:
+        return "C++11";
+        break;
+    }
+
+}
+
+void Interface::PushTo(std::vector<std::string>& list, const std::string& value)
+{
+    bool found = std::find(list.begin(), list.end(), value) != list.end();
+    if(!found)
+    {
+        list.push_back(value);
+    }
+}
+
+void Interface::PushSourceDir(const std::string& dir)  { PushTo(s_SourceDirs, dir);  }
+void Interface::PushIncludeDir(const std::string& dir) { PushTo(s_IncludeDirs, dir); }
+void Interface::PushLibDir(const std::string& dir)     { PushTo(s_LibDirs, dir);     }
+void Interface::PushLibLink(const std::string& lib)    { PushTo(s_LibLinks, lib);    }
+void Interface::PushDefine(const std::string& name)    { PushTo(s_Defines, name);    }
+
+void Interface::SetStandard(Standard Standard)   { s_Standard = Standard; }
+void Interface::SetDebugFlag(bool flag)          { s_DebugFlag = flag; }
+void Interface::SetOutputDir(std::string& dir)   { s_OutputDir = dir;  }
+void Interface::SetOutputName(std::string& name) { s_OutputName = name; }
+
+void Interface::PrintInclude()
+{
+    auto& includeDirs = GetIncludeDirs();
+    for(auto& dir : includeDirs)
+    {
+        std::cout << dir << std::endl;
+    }
+}
+
+void Interface::PrintLibDir()
+{
+    auto& libDirs = GetLibDirs();
+    for(auto& dir : libDirs)
+    {
+        std::cout << dir << std::endl;
+    }
+}
+
+void Interface::PrintLibLink()
+{
+    auto& libLinks = GetLibLinks();
+    for(auto& dir : libLinks)
+    {
+        std::cout << dir << std::endl;
+    }
+}
+
+void Interface::PrintDefine()
+{
+    auto& defines = GetDefines();
+    for(auto& dir : defines)
+    {
+        std::cout << dir << std::endl;
+    }
+}
---
+++ interface.h
@@ -0,0 +1,69 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include <vector>
+#include <string>
+
+enum class Standard
+{
+	CPP98 = 98,
+	CPP03 =  3,
+	CPP11 = 11,
+	CPP14 = 14,
+	CPP17 = 17,
+	CPP20 = 20,
+	CPP23 = 23
+};
+
+class Interface
+{
+
+public:
+	Interface() = delete;
+	Interface(const Interface& other) = delete;
+
+	static const std::vector<std::string>&      GetSourceDirs();	// 1
+	static const std::vector<std::string>& 	    GetIncludeDirs(); 	// 3
+	static const std::vector<std::string>& 	    GetLibDirs();     	// 4
+	static const std::vector<std::string>& 	    GetLibLinks();    	// 5
+	static const std::vector<std::string>& 	    GetDefines();   	// 7
+	static const bool 	    					GetDebugFlag();		// 6
+	static const std::string&  					GetOutputDir();		// 8
+	static const std::string&  					GetOutputName();	// 9
+	static const std::string  					GetStandard();		// 2
+	
+	static void PushTo(std::vector<std::string>& list, const std::string& value);
+
+	static void PushSourceDir(const std::string& dir);
+	static void PushIncludeDir(const std::string& dir);
+	static void PushLibDir(const std::string& dir);
+	static void PushLibLink(const std::string& dir);
+	static void PushDefine(const std::string& dir);
+
+	static void SetStandard(Standard Standard);
+	static void SetDebugFlag(bool flag);
+	static void SetOutputDir(std::string& dir);
+	static void SetOutputName(std::string& name);
+
+
+	// Debug functions
+	static void PrintInclude();
+	static void PrintLibDir();
+	static void PrintLibLink();
+	static void PrintDefine();
+	
+	static void Generate();
+private:
+	static std::vector<std::string> s_SourceDirs;		// 	1
+	static Standard 				s_Standard;			// 	2
+	static std::vector<std::string> s_IncludeDirs;		// 	3	
+	static std::vector<std::string> s_LibDirs;			// 	4
+	static std::vector<std::string> s_LibLinks;	  		//	5	
+	static bool 					s_DebugFlag;        // 	6
+	static std::vector<std::string> s_Defines;			// 	7
+	static std::string 			    s_OutputDir;       	// 	8
+	static std::string 			    s_OutputName;		// 	9
+
+};
+
+#endif
